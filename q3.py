c2992f230756cebe91ad9b55d8722840794f012d
Gorav <goravjindal86@gmail.com>
2024-04-08 10:45:49 +0530

    day2 question3

--- q3.py
+++ q3.py
@@ -1,6 +1,14 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
 if __name__ == '__main__':
-    n = int(input())
-    for i in range(n):
-        print(i+1,end="")
-    
+    n = int(input().strip())
+    print("Weird" if n % 2 != 0 or n % 2 == 0 and 6 <= n <= 20 else "Not Weird")
 
